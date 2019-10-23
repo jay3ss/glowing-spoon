3f3a59e097f8955de1e6dcc8beb63a2f585ff5bb
jay ess <mentoce@gmail.com>
2019-10-22 21:12:12 -0700

    Finish up lab 2

--- labs/lab02/numberguess/script.js
+++ labs/lab02/numberguess/script.js
@@ -1,2 +1,28 @@
 var number = 4;
-var guessesRemaining = 3;
+var guessPrompt = "Please guess a number between 1 and 10: ";
+var loseMessage = "You've exhausted all of your guesses.";
+var wrongMessage = "You didn't guess correctly. ";
+var winMessage = "You guessed the correct number. Congrats!";
+
+var guessAsString = prompt(guessPrompt, "");
+var guess = Number(guessAsString);
+
+if (guess == number) {
+    alert(winMessage);
+} else {
+    guessAsString = prompt(wrongMessage + guessPrompt, "");
+    guess = Number(guessAsString);
+
+    if (guess == number) {
+        alert(winMessage);
+    } else {
+        guessAsString = prompt(wrongMessage + guessPrompt, "");
+        guess = Number(guessAsString);
+
+        if (guess == number) {
+            alert(winMessage);
+        } else {
+            alert(loseMessage);
+        }
+    }
+}
