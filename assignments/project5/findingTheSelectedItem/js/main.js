fd13198de05ba48523bba4ae5b174d71d0ec11bc
jay ess <mentoce@gmail.com>
2019-11-15 15:07:37 -0800

    Use === instead of ==

--- assignments/project5/findingTheSelectedItem/js/main.js
+++ assignments/project5/findingTheSelectedItem/js/main.js
@@ -4,7 +4,7 @@ function showStuff(el) {
     var elementID = document.getElementById(el);
 
     for (i = 0; i < elementID.length; i++) {
-        if (elementID[i].selected == true) {
+        if (elementID[i].selected === true) {
             var x = elementID[i].text;
             // document.getElementById('output').innerHTML = x;
         }
