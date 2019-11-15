7001186b9fa7c061631a4c7ed6f35cef0c460883
jay ess <mentoce@gmail.com>
2019-11-15 14:57:27 -0800

    Start 'Finding the Checked Item' lesson

---
+++ assignments/project5/findingTheCheckedItem/js/main.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+function showStuff(el) {
+    var elementID = document.getElementById(el);
+
+    for (i = 0; i < elementID.length; i++) {
+        if (elementID[i].selected == true) {
+            var x = elementID[i].text;
+            // document.getElementById('output').innerHTML = x;
+        }
+    } // end loop
+    return x;
+} // end function
+
+
+function evaluatePage() {
+    var feedbackState = showStuff('state');
+    var feedbackHome = showStuff('home');
+    document.getElementById('output').innerHTML = feedbackState + '<br>' + feedbackHome;
+}
