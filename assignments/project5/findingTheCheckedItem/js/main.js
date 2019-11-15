6a0d3d27786413902c9bd8c97d3914aef250226b
jay ess <mentoce@gmail.com>
2019-11-15 15:05:37 -0800

    Comitting halfway through

--- assignments/project5/findingTheCheckedItem/js/main.js
+++ assignments/project5/findingTheCheckedItem/js/main.js
@@ -4,7 +4,7 @@ function showStuff(el) {
     var elementID = document.getElementById(el);
 
     for (i = 0; i < elementID.length; i++) {
-        if (elementID[i].selected == true) {
+        if (elementID[i].selected === true) {
             var x = elementID[i].text;
             // document.getElementById('output').innerHTML = x;
         }
@@ -13,8 +13,21 @@ function showStuff(el) {
 } // end function
 
 
+function showMore() {
+    var elementID = document.getElementsByName('bulb');
+    console.log(elementID.length);
+    for (i = 0; i < elementID.length; i++) {
+        console.log(elementID[i].checked);
+        console.log(elementID[i].value);
+        if (elementID[i].checked === true) {
+
+        } // end if
+    } // end loop
+} // end function
+
 function evaluatePage() {
     var feedbackState = showStuff('state');
     var feedbackHome = showStuff('home');
+    showMore();
     document.getElementById('output').innerHTML = feedbackState + '<br>' + feedbackHome;
-}
+} // end function
