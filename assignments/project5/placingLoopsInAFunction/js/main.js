69c0e8dcabff83dc1dca0b8c4d867a472fac1042
jay ess <mentoce@gmail.com>
2019-11-15 14:43:35 -0800

    Finish 'Placeing Loops in a Function' lesson

--- assignments/project5/placingLoopsInAFunction/js/main.js
+++ assignments/project5/placingLoopsInAFunction/js/main.js
@@ -1,3 +1,20 @@
 // JavaScript Document
 
+function showStuff(el) {
+    var elementID = document.getElementById(el);
 
+    for (i = 0; i < elementID.length; i++) {
+        console.log(elementID[i].text);
+        console.log(elementID[i].value);
+        console.log(elementID[i].selected);
+    } // end loop
+} // end function
+
+// showStuff('state');
+
+// showStuff('home');
+
+function evaluatePage() {
+    showStuff('state');
+    showStuff('home');
+}
